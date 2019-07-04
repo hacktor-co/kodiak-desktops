e2dd0cf1568bda776746c8e6acbe6266c092e70b
topcoder-mc <topcodermc@gmail.com>
2019-07-05 00:17:37 +0430

    add nothing

---
+++ Fhack/console/console_base.py
@@ -0,0 +1,8 @@
+
+
+def main():
+    print("Console base")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Fhack/gui/__init__.py
@@ -0,0 +1,10 @@
+"""
+5/july/2019
+"""
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
