29c7db718a6f1dbad4aa9a85b67a7a6d30882f11
topcoder-mc <topcodermc@gmail.com>
2019-09-08 01:11:14 +0430

    set directory finder plugin structure

--- Fhack/plugins/toolsbox/WebTools/tools/DirectoryFinder/gui/windows/main_window_handler.py
+++ Fhack/plugins/toolsbox/WebTools/tools/DirectoryFinder/gui/windows/main_window_handler.py
@@ -1,36 +1,46 @@
 
 from PyQt5.QtWidgets import (
-    QVBoxLayout, QWidget, QPushButton
+    QVBoxLayout, QWidget, QPushButton,
+    QAction, QMenu, QApplication, QHBoxLayout
 )
+from PyQt5.QtCore import Qt
+
 
 class MainWindowHandler(QWidget):
     def __init__(self, parent=None, parent_layout=None):
         super(MainWindowHandler, self).__init__(parent)
         self.__init_ui__()
 
-    def __add_controls(self, org_layout):
-        a = QPushButton("hello")
-        a.setStyleSheet("""
-            min-width: 100px;
-            max-width: 100px;
-            min-height: 100px;
-            min-height: 100px;
-        """)
-        org_layout.addWidget(a)
-        a = QPushButton("hello")
-        a.setStyleSheet("""
-            min-width: 100px;
-            max-width: 100px;
-            min-height: 100px;
-            min-height: 100px;
-        """)
-        org_layout.addWidget(a)
+    @staticmethod
+    def __add_controls__(org_layout):
+        tool_bar_layout = QHBoxLayout()
+        tool_bar_layout.addStretch()
+        tool_bar_layout.setContentsMargins(0, 0, 0, 0)
+        tool_bar_layout.setAlignment(Qt.AlignTop)
+
+        button_new_workshop = QPushButton("1")
+        button_new_workshop.setStyleSheet(
+            """
+                min-width: 100px;
+                min-height: 100px;
+                max-width: 100px;
+                max-height: 100px;
+                background-color: white;
+                color: black;
+            """
+        )
+
+        tool_bar_layout.addWidget(button_new_workshop)
+
+        org_layout.addLayout(tool_bar_layout)
 
     def __init_ui__(self):
         main_layout = QVBoxLayout()
         main_layout.addStretch()
         main_layout.setContentsMargins(0, 0, 0, 0)
-        self.__add_controls(main_layout)
+        main_layout.setAlignment(Qt.AlignJustify)
+
+        self.__add_controls__(main_layout)
 
         self.setLayout(main_layout)
 
--- Fhack/plugins/toolsbox/WebTools/tools/DirectoryFinder/gui_handler.py
+++ Fhack/plugins/toolsbox/WebTools/tools/DirectoryFinder/gui_handler.py
@@ -4,9 +4,10 @@
     - tool for finding directory of web applications
 """
 
-from PyQt5.QtCore import QRect, Qt
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-    QMainWindow, QApplication, QHBoxLayout, QWidget
+    QMainWindow, QHBoxLayout, QWidget,
+    QAction, QMenu
 )
 
 from .gui.windows.main_window_handler import MainWindowHandler
@@ -17,9 +18,11 @@ class MainWindow(QMainWindow):
 
     def init(self, parent):
         super(MainWindow, self).__init__(parent)
-        self.setWindowTitle("Directory finder")
+        self.setWindowTitle("Directory Finder")
         self.setStyleSheet(main_window_style)
-        self.setLayoutDirection(Qt.LeftToRight)
+        self.setLayoutDirection(Qt.RightToLeft)
+        self.setContentsMargins(0, 0, 0, 0)
+
         self.__add_widgets__()
 
     def __add_widgets__(self):
@@ -29,7 +32,6 @@ class MainWindow(QMainWindow):
         main_layout.setSpacing(0)
         main_layout.setContentsMargins(0, 0, 0, 0)
         main_layout.addStretch()
-        main_layout.setAlignment(Qt.AlignTop | Qt.AlignRight)
 
         mainwindow_handler = MainWindowHandler(self, parent_layout=main_layout)
 
@@ -39,4 +41,5 @@ class MainWindow(QMainWindow):
 
     def execute_app(self, parent=None):
         self.init(parent)
+
         self.show()
