3b3e3e1615bf201814301883ca4790f426f6a858
hacktorco <hacktorco@gmail.com>
2019-11-25 03:00:05 +0330

    delete some stuff

--- hyper/plugins/toolsbox/WebTools/tools/UrlFuzzer/tool_api/common/constants.py
+++
@@ -1,4 +0,0 @@
-WEB_HEADERS = {
-    "user-agent": "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) " + \
-                  "AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25"
-}
